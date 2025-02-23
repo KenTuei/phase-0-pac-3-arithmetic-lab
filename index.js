0c94e5d75cc20c140a98a557ae64a5c0c810e9e8
Ken Tuei <kentuei05@gmail.com>
2025-02-23 14:54:52 +0300

    Practice writing functions that do things with numbers

---
+++ index.js
@@ -0,0 +1,179 @@
+function add() {}
+module.exports = { add };
+function subtract() {}
+
+module.exports = { add, subtract };
+function add() {}
+
+function subtract() {}
+
+function multiply() {}
+module.exports = { add, subtract, multiply };
+
+function add() {}
+
+function subtract() {}
+
+function multiply() {}
+
+function divide() {}
+module.exports = { add, subtract, multiply, divide };
+
+function add(a, b) {
+    return a + b;
+  }
+  
+  function subtract(a, b) {
+    return a - b;
+  }
+  
+  function multiply(a, b) {
+    return a * b;
+  }
+  
+  function divide(a, b) {
+    return a / b;
+  }
+  module.exports = { add, subtract, multiply, divide };
+
+  function add(a, b) {
+    return a + b;
+  }
+  
+  function subtract(a, b) {
+    return a - b;
+  }
+  
+  function multiply(a, b) {
+    return a * b;
+  }
+  
+  function divide(a, b) {
+    return a / b;
+  }
+  
+  function increment(n) {
+    return n + 1;
+  }
+  module.exports = { add, subtract, multiply, divide, increment };
+  
+  function add(a, b) {
+    return a + b;
+  }
+  
+  function subtract(a, b) {
+    return a - b;
+  }
+  
+  function multiply(a, b) {
+    return a * b;
+  }
+  
+  function divide(a, b) {
+    return a / b;
+  }
+  
+  function increment(n) {
+    return n + 1;
+  }
+  
+  function decrement(n) {
+    return n - 1;
+  }
+  module.exports = { add, subtract, multiply, divide, increment, decrement };
+  
+  function add(a, b) {
+    return a + b;
+  }
+  
+  function subtract(a, b) {
+    return a - b;
+  }
+  
+  function multiply(a, b) {
+    return a * b;
+  }
+  
+  function divide(a, b) {
+    return a / b;
+  }
+  
+  function increment(n) {
+    return n + 1;
+  }
+  
+  function decrement(n) {
+    return n - 1;
+  }
+  
+  function makeInt(n) {
+    return parseInt(n, 10);
+  }
+  module.exports = { add, subtract, multiply, divide, increment, decrement, makeInt };
+  
+  function add(a, b) {
+    return a + b;
+  }
+  
+  function subtract(a, b) {
+    return a - b;
+  }
+  
+  function multiply(a, b) {
+    return a * b;
+  }
+  
+  function divide(a, b) {
+    return a / b;
+  }
+  
+  function increment(n) {
+    return n + 1;
+  }
+  
+  function decrement(n) {
+    return n - 1;
+  }
+  
+  function makeInt(n) {
+    return parseInt(n, 10);
+  }
+  
+  function preserveDecimal(n) {
+    return parseFloat(n);
+  }
+  module.exports = { add, subtract, multiply, divide, increment, decrement, makeInt, preserveDecimal };
+  
+  function add(a, b) {
+    return a + b;
+  }
+  
+  function subtract(a, b) {
+    return a - b;
+  }
+  
+  function multiply(a, b) {
+    return a * b;
+  }
+  
+  function divide(a, b) {
+    return a / b;
+  }
+  
+  function increment(n) {
+    return n + 1;
+  }
+  
+  function decrement(n) {
+    return n - 1;
+  }
+  
+  function makeInt(n) {
+    return parseInt(n, 10);
+  }
+  
+  function preserveDecimal(n) {
+    return parseFloat(n);
+  }
+  module.exports = { add, subtract, multiply, divide, increment, decrement, makeInt, preserveDecimal };
+  
